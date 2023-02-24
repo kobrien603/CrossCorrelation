b1a42b49d1bf1158102386fd8d76a9a20cf42da4
kobrien603 <okieran603@gmail.com>
2023-02-24 15:17:00 -0500

    loop to build test data. Provide two arrays and run against each correlation option. Using FSharp.Stats lib

--- CrossCorrelation.Console/Program.cs
+++ CrossCorrelation.Console/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using CrossCorrelation;
+using static FSharpAux.Colors.ColorComponent;
+
 Console.WriteLine("Hello, World!");
+var sampleA = new List<double>();
+var sampleB = new List<double>();
+var weight = new List<double>();
+
+for (int i = 0; i < 300; i++)
+{
+    Console.WriteLine($"row: {i + 1}");
+
+    sampleA = Enumerable.Range(1, 300).Select(p => (double)(p * (i + 1) / 10000M)).ToList();
+    sampleB = Enumerable.Range(1, 300).Select(p => (double)(p * (i + 5) / 10000M)).ToList();
+    weight = Enumerable.Range(1, 300).Select(p => (double)(p * (i + 1) / 10000M)).ToList();
+
+    // call F# helper to run correlation
+    CrossCorrelationHelper.RunCrossCorrelation(sampleA.ToArray(), sampleB.ToArray(), weight.ToArray());
+    Console.WriteLine("");
+}
+
+
+    
---
+++ CrossCorrelation/CrossCorrelationHelper.fs
@@ -0,0 +1,22 @@
+﻿namespace CrossCorrelation
+
+open FSharp.Stats
+open FSharp.Stats.Correlation
+
+module CrossCorrelationHelper =
+
+    // function to run correlation
+    let RunCrossCorrelation (a : array<float>, b: array<float>, weight : array<float>) = 
+
+        let pearson = Seq.pearson a b
+        let pearsonW = Seq.pearsonWeighted  a b weight
+        let spearman = Seq.spearman a b
+        let kendall = Seq.kendall a b
+        let bicor = Seq.bicor a b
+
+        printfn($"Pearson: {pearson}");
+        printfn($"Pearson W: {pearsonW}");
+        printfn($"Spearman: {spearman}");
+        printfn($"Kendall: {kendall}");
+        printfn($"Bicor: {bicor}");
+
