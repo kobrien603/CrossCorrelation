9b1ffb1067decdd91093ee30c6492c5a9b8eb5eb
Kieran O'Brien <kobrien603@gmail.com>
2023-03-06 21:42:41 -0500

    add lead lag

--- CrossCorrelation.Console/Program.cs
+++ CrossCorrelation.Console/Program.cs
@@ -1,23 +1,27 @@
 ﻿using CrossCorrelation;
+using static FSharp.Stats.Interpolation.CubicSpline;
 using static FSharpAux.Colors.ColorComponent;
 
 Console.WriteLine("Hello, World!");
-var sampleA = new List<double>();
-var sampleB = new List<double>();
+//List<double> sampleA = sampleA = Enumerable.Range(1, 300).Select(p => (double)(p * (5 * 5.54M))).ToList();
+//List<double> sampleB = Enumerable.Range(1, 300).Select(p => (double)(p * (2 + 4.3M))).ToList();
+List<double> sampleA = sampleA = Enumerable.Range(1, 300).Select(p => (double)p).ToList();
+List<double> sampleB = Enumerable.Range(1, 300).Select(p => (double)p).ToList();
 var weight = new List<double>();
 
-for (int i = 0; i < 300; i++)
-{
-    Console.WriteLine($"row: {i + 1}");
+CrossCorrelationHelper.RunCrossCorrelation(sampleA.ToArray(), sampleB.ToArray(), 300);
 
-    sampleA = Enumerable.Range(1, 300).Select(p => (double)(p * (i + 1) / 10000M)).ToList();
-    sampleB = Enumerable.Range(1, 300).Select(p => (double)(p * (i + 5) / 10000M)).ToList();
-    weight = Enumerable.Range(1, 300).Select(p => (double)(p * (i + 1) / 10000M)).ToList();
+//for (int i = 0; i < 300; i++)
+//{
+//    Console.WriteLine($"row: {i + 1}");
 
-    // call F# helper to run correlation
-    CrossCorrelationHelper.RunCrossCorrelation(sampleA.ToArray(), sampleB.ToArray(), weight.ToArray());
-    Console.WriteLine("");
-}
+//    sampleA = Enumerable.Range(1, 300).Select(p => (double)(p * (i + 1) / 10000M)).ToList();
+//    sampleB = Enumerable.Range(1, 300).Select(p => (double)(p * (i + 5) / 10000M)).ToList();
+//    weight = Enumerable.Range(1, 300).Select(p => (double)(p * (i + 1) / 10000M)).ToList();
+
+//    // call F# helper to run correlation
+//    CrossCorrelationHelper.RunCrossCorrelation(sampleA.ToArray(), sampleB.ToArray(), weight.ToArray());
+//    Console.WriteLine("");
+//}
 
 
-    
--- CrossCorrelation/CrossCorrelationHelper.fs
+++ CrossCorrelation/CrossCorrelationHelper.fs
@@ -6,17 +6,38 @@ open FSharp.Stats.Correlation
 module CrossCorrelationHelper =
 
     // function to run correlation
-    let RunCrossCorrelation (a : array<float>, b: array<float>, weight : array<float>) = 
+    let RunCrossCorrelation (data1 : float[]) (data2 : float[]) (range : int) =
+        let crossCorrelation = Array.zeroCreate<float> (range * 2 + 1)
+        for i in -range .. range do
+            let offsetData2 = Array.zeroCreate<float> data2.Length
+            for j in 0 .. data2.Length - 1 do
+                let k = j + i
+                if k >= 0 && k < data2.Length then
+                    offsetData2.[j] <- data2.[k]
+            crossCorrelation.[i + range] <- pearson data1 offsetData2;
+            printfn "Lag: %d Cross-correlation: %A" i crossCorrelation.[i + range];
+        crossCorrelation
 
-        let pearson = Seq.pearson a b
-        let pearsonW = Seq.pearsonWeighted  a b weight
-        let spearman = Seq.spearman a b
-        let kendall = Seq.kendall a b
-        let bicor = Seq.bicor a b
+        //let lagRange = [-300..300];
+        //for lag in lagRange do
+        //    let shiftedX, shiftedY = 
+        //        if lag < 0 then 
+        //            Array.append<float> a (Array.map (fun _ -> 0) [|1..-lag|]), Array.append<float> b (Array.map (fun _ -> 0) [|1..-lag|])
+        //        elif lag > 0 then 
+        //            Array.append<float> (Array.map (fun _ -> 0) [|1..lag|]) a, Array.append<float> (Array.map (fun _ -> 0) [|1..lag|]) b
+        //        else 
+        //            a, b
+        //    let corr = Seq.pearson shiftedX shiftedY
+        //    printfn "Lag: %d, Cross-correlation: %A" lag corr
+        //let pearson = Seq.pearson a b
+        //let pearsonW = Seq.pearsonWeighted  a b weight
+        //let spearman = Seq.spearman a b
+        //let kendall = Seq.kendall a b
+        //let bicor = Seq.bicor a b
 
-        printfn($"Pearson: {pearson}");
-        printfn($"Pearson W: {pearsonW}");
-        printfn($"Spearman: {spearman}");
-        printfn($"Kendall: {kendall}");
-        printfn($"Bicor: {bicor}");
+        //printfn($"Pearson: {pearson}");
+        //printfn($"Pearson W: {pearsonW}");
+        //printfn($"Spearman: {spearman}");
+        //printfn($"Kendall: {kendall}");
+        //printfn($"Bicor: {bicor}");
 
